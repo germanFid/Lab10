5d3b812eae782d6ced9bc7e4babc0abe3895386f
IEUser <IEUser@MSEDGEWIN10>
2022-05-28 15:02:01 -0700

    Add project files.

---
+++ Project1/file.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#define FILE_OP_SUCCESS 0
+
+#define STR_BUFFER_LEN 250
+
+FILE* openFile(char* filename)
+{
+	FILE* fp = NULL;
+	int result;
+
+	result = fopen_s(&fp, filename, "r+");
+	if (result != FILE_OP_SUCCESS)
+	{
+		if (result == EACCES)
+		{
+			printf("Cannot open this file with r+ attributes due to access failure!\n");
+			return NULL;
+		}
+
+		printf("File Is not found, creating new...\n");
+		result = fopen_s(&fp, filename, "w+");
+
+		if (result != FILE_OP_SUCCESS)
+		{
+			if (result == EACCES)
+			{
+				printf("Cannot open this file with r+ attributes due to access failure!\n");
+			}
+
+			if (result == EFAULT)
+			{
+				printf("Provided adress is incorrect!\n");
+			}
+
+			printf("Cannot create file!\n");
+			return NULL;
+		}
+
+		if (fp == NULL)
+		{
+			printf("File creation failure!\n");
+			return NULL;
+		}
+	}
+	
+	printf("SUCCESS!\n");
+	return fp;
+}
+
+int printFile(FILE* ptr) // printing file line by line
+{
+	char* buf = malloc(STR_BUFFER_LEN);
+
+	fseek(ptr, 0, SEEK_SET);
+	while (fgets(buf, STR_BUFFER_LEN, ptr)) {
+		printf("%s\n", buf);
+	}
+
+	int err;
+	if (err = ferror(ptr))
+	{
+		return err;
+	}
+
+	return 0;
+}
---
+++ Project1/ui.h
@@ -0,0 +1,151 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "file.h"
+
+#define MENU_CHOICES_NUM 5
+#define PATH_DEFAULT_SIZE 250
+#define GETSTRING_ERR_RE -1
+#define GETSTRING_ERR_OVERFLOW -2
+
+void clearExtraInput()
+{
+	int c;
+	while ((c = getchar()) != '\n' && c != EOF) {}
+}
+
+int getIntChoice(int n, int* choices)
+{
+	char buf[3];
+
+	scanf_s("%3s", buf, 3);
+	clearExtraInput();
+
+	char* end;
+	int choice = (int)strtol(buf, &end, 10);
+
+	if (choice == 0)
+		return -1;
+
+	for (size_t i = 0; i < n; i++)
+	{
+		if (choices[i] == choice)
+			return choice;
+	}
+
+	return -1;
+}
+
+void waitForKey()
+{
+	system("pause");
+}
+
+int getString(char* str, const int size)
+{
+	char* buf = (char*) malloc(size + 1);
+	if (gets_s(buf, size) == NULL)
+	{
+		return GETSTRING_ERR_RE;
+	}
+
+	if (strlen(buf) == size)
+	{
+		return GETSTRING_ERR_OVERFLOW;
+	}
+
+	strcpy_s(str, size, buf);
+	free(buf);
+	
+	return 0;
+}
+
+FILE* openFileHandler(char* filename)
+{
+	int result;
+
+	FILE* ptr;
+
+	system("cls");
+	printf("Enter path to file (less than 250 symbols!)\n");
+	
+	do
+	{
+		printf("\n>");
+		result = getString(filename, PATH_DEFAULT_SIZE);
+
+		if (result == GETSTRING_ERR_OVERFLOW)
+		{
+			printf("You've entered too many symbols!\n");
+		}
+
+		if (result == GETSTRING_ERR_RE)
+		{
+			printf("Reading Error!\n");
+		}
+
+	} while (result != 0);
+
+	ptr = openFile(filename);
+
+	waitForKey();
+	return ptr;
+}
+
+void displayFileHandler(FILE* ptr, char* filename)
+{
+	int fileOpenedFlag = 0;
+	if (ptr == NULL)
+	{
+		ptr = openFileHandler(filename);
+		fileOpenedFlag = 1;
+	}
+
+	system("cls");
+	printf("%s\n"
+		"------------\n", filename);
+	int result = printFile(ptr);
+
+	if (result)
+	{
+		if (result == EACCES)
+		{
+			printf("Access failure!\n");
+		}
+
+		else
+		{
+			printf("Some failure happened!\n");
+		}
+	}
+
+	if (fileOpenedFlag)
+	{
+		fclose(ptr);
+		ptr = NULL;
+	}
+	waitForKey();
+}
+
+void showMenu(FILE* fp, char* currentFileName)
+{
+	if (fp != NULL)
+	{
+		printf("------------\nCurrent File: %s\n------------\n", currentFileName);
+	}
+
+	else
+	{
+		printf("------------\nNo file opened!\n------------\n");
+	}
+
+	printf(
+		"1. Create/Open file\n"
+		"2. Read file\n"
+		"3. Write string to file\n"
+		"4. Change security attributes of the file\n"
+		"99. Exit\n\n>");
+}
